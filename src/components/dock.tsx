071e7e3486d80dcd21d884e78c644372a3326e0c
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-07-22 11:27:55 -0400

    feat: added top-bar layout

---
+++ src/components/dock.tsx
@@ -0,0 +1,23 @@
+import { styled } from "styled-components";
+
+
+const DockContent = styled.div`
+    background: linear-gradient(to bottom, var(--color-007), var(--color-006), var(--color-005));
+    filter: drop-shadow(0 0 .1rem var(--color-008));
+
+    border: 1px solid var(--color-007);
+    border-radius: 10px;
+
+    margin: 5px;
+
+    height: 7%;
+    width: 60%;
+`
+
+const Dock = () => {
+    return (
+        <DockContent></DockContent>
+    );
+}
+ 
+export default Dock;
